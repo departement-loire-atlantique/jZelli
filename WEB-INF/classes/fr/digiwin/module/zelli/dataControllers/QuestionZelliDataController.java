8b67240a35f212a3804ad944adc172bcb3195b7e
paul604 <paul604@users.noreply.github.com>
2022-11-23 17:44:38 +0100

    ZEL-85 Notification pour gestionnaires questions

--- WEB-INF/classes/fr/digiwin/module/zelli/dataControllers/QuestionZelliDataController.java
+++ WEB-INF/classes/fr/digiwin/module/zelli/dataControllers/QuestionZelliDataController.java
@@ -1,7 +1,9 @@
 package fr.digiwin.module.zelli.dataControllers;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Map;
+import java.util.Set;
 
 import org.apache.log4j.Logger;
 
@@ -14,17 +16,17 @@ import com.jalios.jcms.BasicDataController;
 import com.jalios.jcms.Channel;
 import com.jalios.jcms.ControllerStatus;
 import com.jalios.jcms.Data;
+import com.jalios.jcms.Group;
 import com.jalios.jcms.Member;
 import com.jalios.jcms.alert.Alert;
-import com.jalios.jcms.alert.Alert.Level;
 import com.jalios.jcms.alert.AlertBuilder;
-import com.jalios.jcms.alert.AlertQueryBuilder;
 import com.jalios.jcms.plugin.PluginComponent;
 import com.jalios.util.Util;
 
 import fr.digiwin.module.zelli.firebase.FirebaseMng;
-import generated.QuestionZelli;
+import fr.digiwin.module.zelli.utils.ZelliUtils;
 import generated.EditQuestionZelliHandler;
+import generated.QuestionZelli;
 
 public class QuestionZelliDataController extends BasicDataController implements PluginComponent {
 
@@ -68,15 +70,54 @@ public class QuestionZelliDataController extends BasicDataController implements 
 	 */
 	public void afterWrite(Data data, int op, Member mbr, Map context) {
 
-		if (op != OP_UPDATE) {
-			return;
-		}
-
 		if (!(data instanceof QuestionZelli)) {
 			return;
 		}
 
 		QuestionZelli questionZelli = (QuestionZelli) data;
+        
+        if(op == OP_CREATE) {
+            // Notif custom
+            Group gestQuestRepGrp = channel.getGroup("$jcmsplugin.zelli.groupe.gestionnaires.id");
+            if(Util.notEmpty(gestQuestRepGrp)) {
+                Set<Member> gestQuestRepSet = gestQuestRepGrp.getMemberSet();
+                
+                AlertBuilder alertBuilder = new AlertBuilder(Alert.Level.ACTION, "zelli", "newQuestion", questionZelli,
+                        questionZelli.getAuthor()) {
+                    @Override
+                    protected void addParams(Member recipient, Map<String, String> paramMap, String markup) {
+                        paramMap.put("age", ZelliUtils.getAgeStrFromDateNaissance(
+                                questionZelli.getAuthor().getExtraDBData("extradb.Member.jcmsplugin.zelli.datenaissance")));
+                        paramMap.put("demRef", questionZelli.getReferent() ? "oui" : "non");
+                        paramMap.put("question", questionZelli.getQuestion());
+                        
+                        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+                        String date = "";
+                        try {
+                            date = sdf.format(questionZelli.getCdate());
+                        } catch (Exception e) {
+                            LOGGER.error("parse date", e);
+                            date = "-1";
+                        }
+                        paramMap.put("date", date);
+
+                        if (HTML_MARKUP.equals(markup)) {
+                            paramMap.put("linkQuest", "<a href=\"" + channel.getUrl()
+                                    + "plugins/ZelliPlugin/jsp/questions.jsp\">Répondre à sa question</a>");
+                        } else {
+                            paramMap.put("linkQuest", "Répondre à sa question : " + channel.getUrl()
+                                    + "plugins/ZelliPlugin/jsp/questions.jsp");
+                        }
+                    }
+                };
+                alertBuilder.sendAlert(gestQuestRepSet);
+                // TODO
+            }
+        }
+
+        if (op != OP_UPDATE) {
+            return;
+        }
 
 		if (Util.notEmpty(questionZelli.getReponse()) && questionZelli.getReponse().length() > 0) {
 			Object dataPrevious = context.get(CTXT_PREVIOUS_DATA);
