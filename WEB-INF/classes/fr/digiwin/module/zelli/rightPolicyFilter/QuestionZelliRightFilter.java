ea3ce27bf447838181939484912ade342f3d2fbe
paul604 <paul604@users.noreply.github.com>
2023-01-06 16:47:31 +0100

    add policyfilter for QuestionZelli

---
+++ WEB-INF/classes/fr/digiwin/module/zelli/rightPolicyFilter/QuestionZelliRightFilter.java
@@ -0,0 +1,36 @@
+package fr.digiwin.module.zelli.rightPolicyFilter;
+
+import com.jalios.jcms.Channel;
+import com.jalios.jcms.Group;
+import com.jalios.jcms.Member;
+import com.jalios.jcms.Publication;
+import com.jalios.jcms.policy.BasicRightPolicyFilter;
+import com.jalios.util.Util;
+
+import generated.QuestionZelli;
+
+public class QuestionZelliRightFilter extends BasicRightPolicyFilter {
+
+    @Override
+    public boolean canBeReadBy(boolean isAuthorized, Publication pub, Member mbr, boolean searchInGroups) {
+        if (!isAuthorized || mbr.isAdmin()) {
+            return isAuthorized;
+        }
+
+        if (!(pub instanceof QuestionZelli) || mbr.isAdmin(pub.getWorkspace())) {
+            return isAuthorized;
+        }
+        
+        if(pub.getAuthor().equals(mbr)) {
+            return isAuthorized;
+        }
+        
+        Group gestQuestRepGrp = Channel.getChannel().getGroup("$jcmsplugin.zelli.groupe.gestionnaires.id");
+        if(searchInGroups && Util.notEmpty(gestQuestRepGrp) && mbr.belongsToGroup(gestQuestRepGrp)) {
+            return isAuthorized;
+        }
+
+        return false;
+    }
+
+}
